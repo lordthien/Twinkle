3cf004184e18427c4f78c198fe662793eabda816
lordthien <nnthien.242@gmail.com>
2021-05-27 22:19:46 +0700

    UPDATE 27/5

--- App.js
+++ App.js
@@ -1,26 +1,10 @@
-import { StatusBar } from "expo-status-bar";
 import React, { Component } from "react";
-import { View, SafeAreaView, Platform } from "react-native";
-
-// import Login from "./code/SignInTwinkle/Login";
-// import SignUp from "./code/SignInTwinkle/SignUpTwinkle/SignUp";
-// import OTP from "./code/SignInTwinkle/OTPAuthen/OTP";
-// import PasswordRecovery from "./code/SignInTwinkle/PasswordRecovery/PasswordRecovery";
-// import ResetPassword from "./code/SignInTwinkle/PasswordRecovery/ResetPassword";
-// import ResetPasswordComplete from "./code/SignInTwinkle/PasswordRecovery/ResetPasswordComplete";
-
-import Home01 from "./code/Home/Home/Home01";
-import HomeBarber from "./code/Home/Barber/HomeBarber";
-import Book from "./code/Home/Book/Book";
-
-import SelectDateTime from "./code/Home/Book/ComponentBook/SelectDateTime";
-import InfoBook from "./code/Home/InfoBook/InfoBook";
 
 import AppNavigator from "./AppNavigator";
 
 class App extends Component {
   render() {
-    return <AppNavigator />
+    return <AppNavigator />;
   }
 }
 
